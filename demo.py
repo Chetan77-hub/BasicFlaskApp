71b3b4d08c230f9eed4a0e280b2e8e47bf169cdd
Chetan77-hub <56333502+Chetan77-hub@users.noreply.github.com>
2021-10-11 12:14:42 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Welcome To Flask app Created on 29 DEc 2020"
+  return "Welcome To Flask app Created on 29 DEc 2020 but edited"
 
 if (__name__) == "__main__":
   app.run(host = '0.0.0.0' ,port=5001,debug=True)
